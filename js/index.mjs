e4a6adb9683f84fec17dc88c16d4527beed921fe
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-10-06 16:35:24 +0200

    updates

--- js/index.mjs
+++ js/index.mjs
@@ -2,7 +2,13 @@ import { setRegisterUserFormListener } from "./handlers/register.mjs";
 import { setLoginUserFormListener } from "./handlers/login.mjs";
 import { setUpdatePostListener } from "./handlers/updatePost.mjs";
 import { setCreateNewPostListener } from "./handlers/createPost.mjs";
-// import { getProfile } from "./api/profile/index.mjs";
+import { getPostsFeed } from "./handlers/getPosts.mjs";
+import { getPostFeedUser } from "./handlers/getPosts.mjs";
+import * as post from "./api/posts/index.mjs";
+import { load } from "./storage/index.mjs";
+
+import { getProfile } from "./api/profile/index.mjs";
+
 
 const path = location.pathname;
 
@@ -14,17 +20,31 @@ if (path === `/pages/login.html`) {
   setCreateNewPostListener();
 } else if (path === `/pages/.............`) {
   setUpdatePostListener();
-}
+} 
+
+
+getProfile();
+getPostsFeed();
+getPostFeedUser();
+
+const username = load("profile");
+console.log(username)
 
 
 
+// async function testTemplateOne() {
+//   const posts = await post.getPosts();
+//   const postings = posts.pop();
+//   const container = document.querySelector("#postsFeed");
+//   renderPost(postings, container)
+// }
 
-// getProfile();
+// testTemplateOne()
 
+
+post.getPosts().then(console.log)
+// post.getPost(958).then(console.log)
 
 // updatePost()
-
-// posts.get()
-
 // posts.getOne()
 
