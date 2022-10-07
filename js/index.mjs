3750489222da9a03502e9955205746bdb3155a06
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-10-07 23:30:21 +0200

    updates running functions

--- js/index.mjs
+++ js/index.mjs
@@ -2,13 +2,12 @@ import { setRegisterUserFormListener } from "./handlers/register.mjs";
 import { setLoginUserFormListener } from "./handlers/login.mjs";
 import { setUpdatePostListener } from "./handlers/updatePost.mjs";
 import { setCreateNewPostListener } from "./handlers/createPost.mjs";
-import { getPostsFeed } from "./handlers/getPosts.mjs";
-import { getPostFeedUser } from "./handlers/getPosts.mjs";
-import * as post from "./api/posts/index.mjs";
-import { load } from "./storage/index.mjs";
-
 import { getProfile } from "./api/profile/index.mjs";
+import * as posts from "./handlers/getPosts.mjs";
 
+
+// import { getPost } from "./api/posts/get.mjs";
+// import { load } from "./storage/index.mjs";
 
 const path = location.pathname;
 
@@ -16,35 +15,15 @@ if (path === `/pages/login.html`) {
   setLoginUserFormListener();
 } else if (path === `/pages/register.html`) {
   setRegisterUserFormListener();
-} else if (path === `/pages/profile.html` || `index.html`) {
+} else if (path === `/pages/profile.html`) {
+  getProfile();
   setCreateNewPostListener();
-} else if (path === `/pages/.............`) {
+  posts.getPostFeedUser();
+} else if (path === `/pages/editPost.html`) {
   setUpdatePostListener();
-} 
-
-
-getProfile();
-getPostsFeed();
-getPostFeedUser();
-
-const username = load("profile");
-console.log(username)
-
-
-
-// async function testTemplateOne() {
-//   const posts = await post.getPosts();
-//   const postings = posts.pop();
-//   const container = document.querySelector("#postsFeed");
-//   renderPost(postings, container)
-// }
-
-// testTemplateOne()
-
-
-post.getPosts().then(console.log)
-// post.getPost(958).then(console.log)
-
-// updatePost()
-// posts.getOne()
-
+} else if (path === `/index.html`) {
+  setCreateNewPostListener();
+  posts.getPostsFeed();
+} else if (path === `/pages/singlePost.html`) {
+  posts.getPostSingle();
+}
