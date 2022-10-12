0f87532d8ff8bcd93f8db45109de06e3d15f3fcb
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-10-12 15:44:07 +0200

    formatting

--- js/handlers/login.mjs
+++ js/handlers/login.mjs
@@ -6,16 +6,16 @@ import { login } from "../api/auth/login.mjs";
 export function setLoginUserFormListener() {
   const form = document.querySelector("#loginUserForm");
 
-  form.addEventListener("submit", event => {
-    event.preventDefault();
-  
-    const form = event.target;
-    const formData = new FormData(form);
-    const profile = Object.fromEntries(formData.entries());
-     
-    login(profile)
+  if (form) {
+    form.addEventListener("submit", event => {
+      event.preventDefault();
+    
+      const form = event.target;
+      const formData = new FormData(form);
+      const profile = Object.fromEntries(formData.entries());
       
-})
-  
+      login(profile)    
+    }) 
+  } 
 }
 
--- js/handlers/register.mjs
+++ js/handlers/register.mjs
@@ -6,15 +6,16 @@ import { register } from "../api/auth/register.mjs";
 export function setRegisterUserFormListener() {
   const form = document.querySelector("#registerUserForm");
 
-  form.addEventListener("submit", event => {
-    event.preventDefault();
-  
-    const form = event.target;
-    const formData = new FormData(form);
-    const profile = Object.fromEntries(formData.entries())
-    // const action = form.action;
-    // const method = form.method;
+  if (form) {
+    form.addEventListener("submit", event => {
+      event.preventDefault();
+    
+      const form = event.target;
+      const formData = new FormData(form);
+      const profile = Object.fromEntries(formData.entries());
 
-    register(profile)
-})
+      register(profile)
+    })
+  }
+  
 }
